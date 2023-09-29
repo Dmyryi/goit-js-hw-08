66a1cb6a4bfac4258294f28f0ba82ff9e6bbc2a3
Dmytryi <muzalevskyid333@gmail.com>
2023-09-29 10:50:41 +0300

    .

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,12 +2,11 @@ import throttle from 'lodash.throttle';
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  input: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
+
 };
 
 const FORM_STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
@@ -15,50 +14,39 @@ refs.form.addEventListener('submit', onFormSubmit);
 fillForm();
 
 function fillForm() {
-  let storageFormData = JSON.parse(localStorage.getItem(FORM_STORAGE_KEY));
-
-  // Перевірка чи є дані в локалсторедж по всіх полях
-  if (!storageFormData) {
-    return;
-  }
-
-  // спочатку зберігаємо дані зі стореджа в об'єкт
-  formData.email = storageFormData.email ? storageFormData.email : '';
-  formData.message = storageFormData.message ? storageFormData.message : '';
-
-  // Потім заповнюємо форму даними з об'єкту
-  refs.input.value = formData.email;
-  refs.textarea.value = formData.message;
-
+try {
+  const savedData = localStorage.getItem(FORM_STORAGE_KEY, 500);
+  if (!savedData) return;
+  formData = JSON.parse(savedData)
+  Object.entries(formData).forEach(([key, val]) => {
+    refs.form.elements[key].value = val;
+  })
+} catch ({message}) {
+  console.log(message)
+}
 
 }
 
 function onFormInput(e) {
-  formData[e.target.name] = e.target.value;
+  formData[e.target.name] = e.target.value.trim();
   localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
 
-  if (refs.input.value === '' || refs.textarea.value === '') {
-    alert('Hey! All fields must be filled! ;-)');
-  } else {
-    // Очищуємо сторедж
+ 
+
     localStorage.removeItem(FORM_STORAGE_KEY);
+  console.log(formData);
+  formData = {}
+  e.target.reset()
 
-    // Записуем поточні значення форми
-    formData.email = refs.input.value;
-    formData.message = refs.textarea.value;
 
-    // Очищаємо форму
-    refs.input.value = '';
-    refs.textarea.value = '';
 
-    // Виводимо в консоль объект з поточними даними
 
     delete formData.email;
     delete formData.message;
     
-  }
+  
 }
